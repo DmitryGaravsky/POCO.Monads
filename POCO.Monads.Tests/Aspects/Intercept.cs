35c274a7de6ce2b6cb1dd60c70131bb5349a7895
DmitryGaravsky <garavskydmitry@gmail.com>
2017-06-15 23:42:51 +0300

    Intercept for Virtual Methods
    Intercept for Virtual Methods

--- POCO.Monads.Tests/Aspects/Intercept.cs
+++ POCO.Monads.Tests/Aspects/Intercept.cs
@@ -3,7 +3,6 @@
     using System.Linq.Expressions;
     using System.Reflection;
     using NUnit.Framework;
-
     #region Test Classes
     class Foo {
         public void Greet() {
@@ -15,6 +14,9 @@
         public static string GetClass() {
             return "Foo";
         }
+        public virtual string GetFoo() {
+            return "@Foo";
+        }
     }
     class Bar {
         public void Greet() {
@@ -25,6 +27,9 @@
         }
         public static string GetClass() {
             return "Bar";
+        }
+        public virtual string GetBar() {
+            return "@Bar";
         }
     }
     #endregion Test Classes
@@ -61,6 +66,21 @@
             Assert.AreEqual("foo", foo.GetName());
             Assert.AreEqual("bar", bar.GetName());
         }
+        [Test, Explicit]
+        public void Test00_Instance_Virtual() {
+            Foo foo = new Foo();
+            Bar bar = new Bar();
+            var method = typeof(Foo).GetMethod("GetFoo");
+            var replacement = typeof(Bar).GetMethod("GetBar");
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("@Bar", bar.GetBar());
+            using(method.@Intercept(replacement)) {
+                Assert.AreEqual("@Bar", foo.GetFoo());
+                Assert.AreEqual("@Bar", bar.GetBar());
+            }
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("@Bar", bar.GetBar());
+        }
         [Test]
         public void Test01_Instance_Expression() {
             Foo foo = new Foo();
@@ -76,8 +96,52 @@
             Assert.AreEqual("foo", foo.GetName());
             Assert.AreEqual("bar", bar.GetName());
         }
+        [Test]
+        public void Test02_Expression_VirtualToNonVirtual() {
+            Foo foo = new Foo();
+            Bar bar = new Bar();
+            Expression<Func<Foo, string>> method = x => x.GetFoo();
+            Expression<Func<Bar, string>> replacement = x => x.GetName();
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("bar", bar.GetName());
+            using(method.@Intercept(replacement)) {
+                Assert.AreEqual("bar", foo.GetFoo());
+                Assert.AreEqual("bar", bar.GetName());
+            }
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("bar", bar.GetName());
+        }
+        [Test]
+        public void Test02_Expression_VirtualToStatic() {
+            Foo foo = new Foo();
+            Expression<Func<Foo, string>> method = x => x.GetFoo();
+            Expression<Func<string>> replacement = () => Bar.GetClass();
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("Bar", Bar.GetClass());
+            using(method.@Intercept(replacement)) {
+                Assert.AreEqual("Bar", foo.GetFoo());
+                Assert.AreEqual("Bar", Bar.GetClass());
+            }
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("Bar", Bar.GetClass());
+        }
+        [Test]
+        public void Test02_Expression_VirtualToVirtual() {
+            Foo foo = new Foo();
+            Bar bar = new Bar();
+            Expression<Func<Foo, string>> method = x => x.GetFoo();
+            Expression<Func<Bar, string>> replacement = x => x.GetBar();
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("@Bar", bar.GetBar());
+            using(method.@Intercept(replacement)) {
+                Assert.AreEqual("@Bar", foo.GetFoo());
+                Assert.AreEqual("@Bar", bar.GetBar());
+            }
+            Assert.AreEqual("@Foo", foo.GetFoo());
+            Assert.AreEqual("@Bar", bar.GetBar());
+        }
         [Test, Explicit]
-        public void Test02_Static() {
+        public void Test03_Static() {
             var method = typeof(Foo).GetMethod("GetClass");
             var replacement = typeof(Bar).GetMethod("GetClass");
             Assert.AreEqual("Foo", Foo.GetClass());
@@ -90,7 +154,7 @@
             Assert.AreEqual("Bar", Bar.GetClass());
         }
         [Test]
-        public void Test02_Static_Expression() {
+        public void Test03_Static_Expression() {
             Expression<Func<string>> method = () => Foo.GetClass();
             Expression<Func<string>> replacement = () => Bar.GetClass();
             Assert.AreEqual("Foo", Foo.GetClass());
--- POCO.Monads/Internal/RuntimeMethodHelper.cs
+++ POCO.Monads/Internal/RuntimeMethodHelper.cs
@@ -1,25 +1,45 @@
 namespace POCO.Monads.Internal {
     using System;
-    using System.Diagnostics;
     using System.Reflection;
     using System.Runtime.CompilerServices;
     using System.Runtime.InteropServices;
 
     static class RuntimeMethodHelper {
-        readonly static int METHOD_OFFSET = 8;
         internal static IntPtr Replace(MethodBase source, MethodBase destination) {
             RuntimeHelpers.PrepareMethod(source.MethodHandle);
             RuntimeHelpers.PrepareMethod(destination.MethodHandle);
-            return Replace(source.MethodHandle, destination.MethodHandle);
+            return Replace(GetMethodPtr(source), GetMethodPtr(destination));
         }
         internal static void Restore(MethodBase destination, IntPtr originalValue) {
-            Marshal.WriteIntPtr(destination.MethodHandle.Value, METHOD_OFFSET, originalValue);
+            Marshal.WriteIntPtr(GetMethodPtr(destination), originalValue);
         }
-        static IntPtr Replace(RuntimeMethodHandle source, RuntimeMethodHandle destination) {
-            IntPtr originalValue = Marshal.ReadIntPtr(destination.Value, METHOD_OFFSET);
-            IntPtr value = Marshal.ReadIntPtr(source.Value, METHOD_OFFSET);
-            Marshal.WriteIntPtr(destination.Value, METHOD_OFFSET, value);
+        static IntPtr Replace(IntPtr sourcePtr, IntPtr destinationPtr) {
+            IntPtr originalValue = Marshal.ReadIntPtr(destinationPtr);
+            IntPtr value = Marshal.ReadIntPtr(sourcePtr);
+            Marshal.WriteIntPtr(destinationPtr, value);
             return originalValue;
+        }
+        static IntPtr GetMethodPtr(MethodBase method) {
+            if(method.IsVirtual)
+                return GetVirtualMethodPtr(method);
+            return GetInstanceOrStaticMethodPtr(method);
+        }
+        static IntPtr GetInstanceOrStaticMethodPtr(MethodBase method) {
+            IntPtr methodHandle = method.MethodHandle.Value;
+            const int METHOD_PTR_OFFSET = 8;
+            return (IntPtr.Size == 4) ?
+                new IntPtr(methodHandle.ToInt32() + METHOD_PTR_OFFSET) :
+                new IntPtr(methodHandle.ToInt64() + METHOD_PTR_OFFSET);
+        }
+        static IntPtr GetVirtualMethodPtr(MethodBase method) {
+            const int METHOD_INDEX_OFFSET = 4;
+            int methodIndex = Marshal.ReadByte(method.MethodHandle.Value, METHOD_INDEX_OFFSET);
+            IntPtr typeHandle = method.DeclaringType.TypeHandle.Value;
+            const int TYPE_METHODTABLE_OFFSET_X86 = 10 * sizeof(int);
+            const int TYPE_METHODTABLE_OFFSET_X64 = 8 * sizeof(int);
+            return (IntPtr.Size == 4) ?
+                new IntPtr(Marshal.ReadInt32(typeHandle, TYPE_METHODTABLE_OFFSET_X86) + methodIndex * IntPtr.Size) :
+                new IntPtr(Marshal.ReadInt64(typeHandle, TYPE_METHODTABLE_OFFSET_X64) + methodIndex * IntPtr.Size);
         }
     }
 }
